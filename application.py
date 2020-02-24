c80315bb6c89616f5b360dea4fe00063b7f8f0ac
gongbf <530460414@qq.com>
2020-02-24 22:44:36 +0800

    23
    3

--- application.py
+++ application.py
@@ -7,16 +7,5 @@ import imutils.text
 @app.route("/")
 def hello():
 
-    image = cv2.imread("images/bridge.jpg")
-    text = request.args.get('text');
-   
-    imutils.text.put_centered_text(image,
-                                text,
-                                font_face=cv2.FONT_HERSHEY_SIMPLEX,
-                                font_scale=1,
-                                color=(0, 255, 0),
-                                thickness=2)
-    
-    cv2.imwrite('images/image.jpg',image)
 
     return send_file('images/image.jpg', mimetype='image/gif')
